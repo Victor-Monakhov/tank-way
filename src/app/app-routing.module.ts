ce8b00c7c05b1828be5aeb66c215b1d84ad3befd
Victor Monakhov <Victormonakhov92@gmail.com>
2021-10-25 03:53:29 +0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NAVIGATE } from './app.config';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: `/${NAVIGATE.HOME}`,
+    pathMatch: 'full',
+  },
+  {
+    path: NAVIGATE.HOME,
+    loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/features/home/home.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+  ]
+})
+export class HomeModule { }
