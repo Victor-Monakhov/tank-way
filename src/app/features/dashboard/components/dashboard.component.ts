62d11da934b41c31165a6b763b28f89541e59650
kotoleksii <akkot08@gmail.com>
2022-07-26 13:07:06 +0300

    fix: dashboard component

--- src/app/features/dashboard/components/dashboard.component.ts
+++ src/app/features/dashboard/components/dashboard.component.ts
@@ -1,20 +1,24 @@
-import {Component, OnInit} from '@angular/core';
-import {UserService} from "../../../shared/services/user.service";
-import {LocalizationService} from "../../../shared/services/internationalization/localization.service";
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {LocalizationService} from '../../../shared/services/internationalization/localization.service';
+import {UserService} from '../../../shared/services/user.service';
+import {IUser} from '../../../shared/interfaces/auth/auth.interface';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
-  users?: any[];
+export class DashboardComponent implements OnInit, AfterViewInit {
+  @ViewChild('dashboard_wrapper') private dashboardWrapper: ElementRef;
+  @ViewChild('mode_icon') private modeIcon: ElementRef;
+
+  public users?: any[];
   public navLinks: Array<any> = [
     {link: '/', icon: 'fa-solid fa-gamepad', name: 'Dashboard'},
     {link: '#', icon: 'fa-solid fa-play', name: 'Play'},
     {link: '#', icon: 'fa-solid fa-message', name: 'Chat'},
     {link: '#', icon: 'fa-solid fa-image', name: 'NFT Gallery'},
-    {link: '#', icon: 'fa-solid fa-sliders', name: 'Settings'},
+    {link: '#', icon: 'fa-solid fa-sliders', name: 'Settings'}
   ];
   public boxes: Array<any> = [
     {icon: 'fa-solid fa-chart-column', title: 'Statistic1', data: '20,123'},
@@ -23,16 +27,18 @@ export class DashboardComponent implements OnInit {
   ]
   public displayedColumns = ['ID', 'Nickname', 'Email', 'Password', 'Token', 'Status', 'Actions'];
 
-  constructor(private userService: UserService, private localizationService: LocalizationService) {
-    //localizationService.useLanguage('en-US');
-
+  public constructor(private userService: UserService, private localizationService: LocalizationService) {
+    // localizationService.useLanguage('en-US');
     for (let index = 0; index < this.navLinks.length; index++) {
       this.navLinks[index].name = 'DASHBOARD.NAV_' + index.toString();
     }
   }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.getUsers();
+  }
+
+  public ngAfterViewInit(): void {
     this.switchThemeMode();
   }
 
@@ -41,26 +47,25 @@ export class DashboardComponent implements OnInit {
       .subscribe({
         next: (data) => {
           this.users = data;
-          console.log(data);
+          // console.log(data);
         },
         error: (e) => console.error(e)
       });
   }
 
-  public getUserById(id: number): any {
-    return this.users.find((el: any) => el.id === id);
+  public getUserById(id: number): IUser {
+    return this.users.find((el: IUser) => el.id === id);
   }
 
   public refreshList(): void {
     this.getUsers();
   }
 
-  public deleteUser(id: number): void {
-    if (confirm(`Are you sure want to delete the user ${this.getUserById(id).nickname} from the database?`)) {
+  public deleteUserById(id: number): void {
+    if (confirm(`Are you sure want to delete the ${this.getUserById(id).nickname.toUpperCase()} from the database?`)) {
       this.userService.delete(id)
         .subscribe({
-          next: (data) => {
-            console.log(data);
+          next: () => {
             this.refreshList();
           },
           error: (e) => console.error(e)
@@ -72,42 +77,39 @@ export class DashboardComponent implements OnInit {
   }
 
   private switchThemeMode(): void {
-    const body = document.querySelector(".dashboard-wrapper");
-    const modeToggle = body.querySelector(".mode-toggle");
+    const modeToggle = this.dashboardWrapper.nativeElement.querySelector('.mode-toggle');
+    const sidebar = this.dashboardWrapper.nativeElement.querySelector('.sidebar');
+    const sidebarToggle = this.dashboardWrapper.nativeElement.querySelector('.sidebar-toggle');
 
-    const sidebar = body.querySelector(".sidebar");
-    const sidebarToggle = body.querySelector(".sidebar-toggle");
+    const getMode = localStorage.getItem('mode');
+    const getStatus = localStorage.getItem('status');
 
-    const getMode = localStorage.getItem("mode");
-    const icon = document.querySelector("#mode-icon");
-
-    if (getMode && getMode === "dark") {
-      body.classList.toggle("dark");
+    if (getMode && getMode === 'dark') {
+      this.dashboardWrapper.nativeElement.classList.toggle('dark');
     }
 
-    let getStatus = localStorage.getItem("status");
-    if (getStatus && getStatus === "close") {
-      sidebar.classList.toggle("close");
-      icon.className = "";
+    if (getStatus && getStatus === 'close') {
+      sidebar.classList.toggle('close');
+      this.modeIcon.nativeElement.className = '';
     }
 
-    modeToggle.addEventListener("click", () => {
-      body.classList.toggle("dark");
-      if (body.classList.contains("dark")) {
-        localStorage.setItem("mode", "dark");
+    modeToggle.addEventListener('click', () => {
+      this.dashboardWrapper.nativeElement.classList.toggle('dark');
+      if (this.dashboardWrapper.nativeElement.classList.contains('dark')) {
+        localStorage.setItem('mode', 'dark');
       } else {
-        localStorage.setItem("mode", "light");
+        localStorage.setItem('mode', 'light');
       }
     });
 
-    sidebarToggle.addEventListener("click", () => {
-      sidebar.classList.toggle("close");
-      if (sidebar.classList.contains("close")) {
-        localStorage.setItem("status", "close");
-        icon.className = "";
+    sidebarToggle.addEventListener('click', () => {
+      sidebar.classList.toggle('close');
+      if (sidebar.classList.contains('close')) {
+        localStorage.setItem('status', 'close');
+        this.modeIcon.nativeElement.className = '';
       } else {
-        localStorage.setItem("status", "open");
-        icon.className = "fa-solid fa-moon";
+        localStorage.setItem('status', 'open');
+        this.modeIcon.nativeElement.className = 'fa-solid fa-moon';
       }
     });
   }
