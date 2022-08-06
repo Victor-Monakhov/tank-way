29e1193e1a8470d2b265c9ad1e29e4007bea70af
kotoleksii <akkot08@gmail.com>
2022-08-06 15:02:54 +0300

    add: gameboard-header & gameboard-buttons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,18 +8,18 @@ const routes: Routes = [
     redirectTo: `/${NAVIGATE.HOME}`,
     pathMatch: 'full'
   },
-  {
-    path: NAVIGATE.HOME,
-    loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule)
-  },
+  // {
+  //   path: NAVIGATE.HOME,
+  //   loadChildren: () => import('./features/home/home.module').then((m) => m.HomeModule)
+  // },
   // {
   //   path: NAVIGATE.HOME,
   //   loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule),
   // }
-  // {
-  //   path: NAVIGATE.HOME,
-  //   loadChildren: () => import('./features/gameboard/gameboard.module').then((m) => m.GameboardModule)
-  // }
+  {
+    path: NAVIGATE.HOME,
+    loadChildren: () => import('./features/gameboard/gameboard.module').then((m) => m.GameboardModule)
+  }
 ];
 
 @NgModule({
--- src/app/features/gameboard/components/gameboard/gameboard.component.ts
+++ src/app/features/gameboard/components/gameboard/gameboard.component.ts
@@ -1,16 +1,19 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-gameboard',
   templateUrl: './gameboard.component.html',
   styleUrls: ['./gameboard.component.scss']
 })
-export class GameboardComponent implements OnInit {
+export class GameboardComponent {
+
+  public navLinks: Array<any> = [
+    {link: '/', icon: 'fa-solid fa-jet-fighter', title: 'Battlegrounds'},
+    {link: '#', icon: 'fa-solid fa-warehouse', title: 'Hangar'},
+    {link: '#', icon: 'fa-solid fa-industry', title: 'Base'},
+    {link: '#', icon: 'fa-solid fa-gear', title: 'Settings'}
+  ];
 
   public constructor() {
   }
-
-  ngOnInit(): void {
-  }
-
 }
