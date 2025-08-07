c86f08b1069a8957f94ac6d5b2c60eaaeb65af78
Victor-Monakhov <victormonakhov92@gmail.com>
2025-08-07 22:22:15 +0300

    release-4.0.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://localhost:5287/api/',
+  googleOAuthClientId: '697267962581-n5sp68g5kdvtr5id9so3h9vm9btek8tl.apps.googleusercontent.com',
+  facebookOAuthAppId: '1353131275805071',
 };
