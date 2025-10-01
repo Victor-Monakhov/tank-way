20b0bb48f36ae3bbcdd99645b4aa9b83f333b883
Victor-Monakhov <victormonakhov92@gmail.com>
2025-10-01 11:14:14 +0300

    fixed bug with breakpoints

--- src/app/shared/services/breakpoint/breakpoint.enum.ts
+++ src/app/shared/services/breakpoint/breakpoint.enum.ts
@@ -1,5 +1,5 @@
 export enum EBreakPointsEnum {
-  DesktopFHD = `(min-width: $1921px)`,
+  DesktopFHD = `(min-width: 1921px)`,
   Desktop = `(min-width: 1201px) and (max-width: 1920px)`,
   Laptops = `(min-width: 993px) and (max-width: 1200px)`,
   Tablets = `(min-width: 577px) and (max-width: 992px)`,
